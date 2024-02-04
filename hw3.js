b313d121b92574a5344dbd6fe2a0f55c02b70ff4
liza <elizavetakulik2@gmail.com>
2024-02-04 21:43:13 +0100

    work

--- hw3.js
+++ hw3.js
@@ -1,43 +1,16 @@
-// const addOption = (+);
-// const subOption = (-);
-// const multOption = (*);
-// const divOption = (/);
+let operation = prompt("Choose the option: add, sub, mult or div");
 
-let userRequest = prompt("Choose the option: add, sub, mult or div");
-let firstDigit = parseInt(prompt('Write first digit'));
-let secondtDigit = parseInt(prompt('Write second digit'));
+let addOpt = 'add';
+let subOpt = 'sub';
+let multOpt = 'mult';
+let divOpt = 'div';
+
+let a = parseInt(prompt('Write first digit'));
+let b = parseInt(prompt('Write second digit'));
+
 let result;
 
-userRequest === "add" && result = firstDigit + secondtDigit;
-userRequest === "sub" && result = firstDigit - secondtDigit;
-userRequest === "mult" && result = firstDigit * secondtDigit;
-userRequest === "div" && result = firstDigit / secondtDigit;
-
-alert(firstDigit + ' ' + userRequest + ' ' + secondtDigit + '=' + result);
-
-// if {
-//     userRequest = "add";
-//     do
-// }
-
-// console.log(typeof(userRequest));
-
-
-
-// addOption = userRequest;
-// subOption = userRequest;
-// multOption = userRequest;
-// divOption = userRequest;
-
-// console.log(true);
-
-// userRequest = 'add';
-// userRequest = 'sub';
-// userRequest = 'mult';
-// userRequest = 'div';
-
-// let userChoice = userRequest(true);
-
-
-
-// alert('Your request result is: ' + (firstDigit + userChoice + secondtDigit));
+operation === addOpt && alert(`Your request is: ${a} + ${b} = ${result = (a + b)}`);
+operation === subOpt && alert(`Your request is: ${a} - ${b} = ${result = (a - b)}`);
+operation === multOpt && alert(`Your request is: ${a} * ${b} = ${result = (a * b)}`);
+operation === divOpt && alert(`Your request is: ${a} / ${b} = ${result = (a / b)}`);
